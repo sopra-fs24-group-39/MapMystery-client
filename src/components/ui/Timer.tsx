3c438c704a377f43fea5141f62be21467516f247
Nils Jacobi <nils@MacBook-Pro-von-Nils.local>
2024-04-18 12:02:56 +0200

    #28, #25: Finished work on the Game component

---
+++ src/components/ui/Timer.tsx
@@ -0,0 +1,27 @@
+import { React, useState, useEffect } from 'react';
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds(prevSeconds => prevSeconds + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const remainingSeconds = time % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  };
+
+  return (
+    <span>
+      Timer: {formatTime(seconds)}
+    </span>
+  );
+};
+
+export default Timer;
--- src/components/views/Game.tsx
+++ src/components/views/Game.tsx
@@ -1,19 +1,55 @@
-import React from "react";
+import { React, useState} from "react";
 import BaseContainer from "components/ui/BaseContainer";
 import "styles/views/Game.scss";
+import "styles/ui/DropDown.scss";
 import Button from "../ui/Button";
 import BackgroundImage from "./sources/background.png";
 import Header from "./Header";
 import Logo from "../pictures/Logo";
+import DropDown from "../ui/DropDown";
+import Timer from "../ui/Timer";
 
 const Game = () => {
+  const [playerMS, setPlayerMS] = useState("");
+  const [gameMode, setGameMode] = useState("");
+  const [mp, setMp] = useState("");
+  const [sp, setSp] = useState("hidden");
+  const [timer, setTimer] = useState("hidden");
+  const [text, setText] = useState("");
+
+  function populateBeforeAPICall() {
+    setGameMode(localStorage.getItem("gamemode"));
+    if (mp === "hidden"){
+      setPlayerMS("Singleplayer");
+    } else {
+      setPlayerMS("Multiplayer");
+    }
+    setTimer("");
+    setText("hidden");
+  }
+
+  function handleMPlayer() {
+    if (sp === "") {
+      setSp("hidden");
+      setMp("");
+    }
+  }
+
+  function handleSPlayer() {
+    if (mp === "") {
+      setSp("");
+      setMp("hidden");
+    }
+  }
+
   return (
     <BaseContainer backgroundImage={BackgroundImage} className="main-body">
       <div className={"center-container left-5"}>
         <Header/>
-        <Logo width="300px" height="300px" className="logo" />
+        <Logo width="200px" height="200px" className="logo" />
         <div className="text-container-sm">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p className={text}> Select your preferences and join a lobby</p>
+          <p className={timer}><Timer></Timer></p>
         </div>
         <div className={"menu-buttons-container"}>
           <Button
@@ -21,21 +57,36 @@ const Game = () => {
             width={"lg"}
             name={"Create Game"}>
           </Button>
-          <Button
-            type={"regular"}
-            width={"lg"}
-            name={"Gamemode Selection"}>
-          </Button>
-          <div className={"menu-mpsp-select"}>
-            <Button
-              type={"regular"}
-              width={"md"}
-              name={"Multiplayer"}>
-            </Button>
-            <Button
-              type={"regular"}
-              width={"md"}
-              name={"Singleplayer"}>
+          <div className={"menu-mpsp-select mt-3"}>
+            <div className={"flex flex-row items-center"}
+                 onClick={handleMPlayer}>
+              <div className={"arrow-right "+mp}></div>
+              <Button
+                type={"regular"}
+                width={"md"}
+                name={"Multiplayer"}>
+              </Button>
+            </div>
+            <div className={"flex flex-row items-center"}
+                 onClick={handleSPlayer}>
+              <div className={"arrow-right "+sp}></div>
+              <Button
+                type={"regular"}
+                width={"md"}
+                name={"Singleplayer"}>
+              </Button>
+            </div>
+          </div>
+          <div className={"mt-3"}>
+            <DropDown defaultValue={"Select Gamemode"}
+                      altValues={["Globe Guesser", "Flag Finder", "Geo Genius"]}>
+            </DropDown>
+          </div>
+          <div className={"mt-3"}
+               onClick={populateBeforeAPICall}>
+            <Button type={"regular"}
+                    width={"lg"}
+                    name={"Start!"}>
             </Button>
           </div>
         </div>
