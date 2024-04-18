790214861d418ffc1ab49094c8409a132c033bd1
Joshua-T-Winterflood <joshua.winterflood@gmail.com>
2024-04-18 09:16:46 +0200

    Adding different locations as a prototype for the panoramaview

--- src/components/views/PanoramaView.tsx
+++ src/components/views/PanoramaView.tsx
@@ -9,11 +9,27 @@ declare global {
 const PanoramaView = () => {
     useEffect(() => {
         // Define initPano inside useEffect to ensure it captures current state and props if needed
+        const locations = [
+            [{lat:47.376644, lng:8.544620},{city:'Zuerich'}],
+            [{lat:-36.854639, lng:174.762781},{city:'Auckland'}],
+            [{lat:55.951537, lng:-3.199094},{city:'Edinburgh'}],
+            [{lat:49.276188, lng:-123.099929},{city:'Vancouver'}],
+            [{lat:-33.918082, lng:18.476921},{city:'Kapstadt'}],
+            [{lat:1.317413, lng: 103.862955},{city:'Singapur'}],
+            [{lat:35.713754, lng:139.779200},{city:'Tokyo'}],
+            [{lat:52.524574, lng:13.349272},{city:'Berlin'}],
+            [{lat:55.773513, lng:37.590209},{city:'Moscow'}],
+            [{lat:-22.957637, lng:-43.199269},{city:'Rio de Janeiro'}],
+        ]
+
         function initPano() {
+            const currentLocation = locations[Math.floor(Math.random()*locations.length)]
+            const currentCoordinates = currentLocation[0]
+            const currentCity = currentLocation[1].city
             const panorama = new google.maps.StreetViewPanorama(
                 document.getElementById("panorama") as HTMLElement,
                 {
-                    position: { lat: 42.345573, lng: -71.098326 },
+                    position: currentCoordinates,
                     addressControlOptions: {
                         position: google.maps.ControlPosition.BOTTOM_CENTER,
                     },
