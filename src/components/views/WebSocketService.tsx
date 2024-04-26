dbfd85aeaa27e5b8058a893f15bfc00bfe59997e
whyKirbyy <tim.schmid02@icloud.com>
2024-04-26 20:33:59 +0200

    main ready for cloud testing X

--- src/components/views/WebSocketService.tsx
+++ src/components/views/WebSocketService.tsx
@@ -8,7 +8,7 @@ class WebSocketService {
   }
 
   initializeWebSocket() {
-    const socket = new SockJS('https://sopra-fs24-group-39-server.oa.r.appspot.com:8081/wss'); //'https://142.251.46.180:443/wss'
+    const socket = new SockJS('https://sopra-fs24-group-39-server.oa.r.appspot.com:8080/ws'); //'https://142.251.46.180:443/wss'
     this.client = new Client({
       webSocketFactory: () => socket,
       debug: (str) => {
