59d0e5057e4aecfd85c3d558b5debcffc6beea54
whyKirbyy <tim.schmid02@icloud.com>
2024-04-26 14:05:22 +0200

    close #32

--- src/components/views/GlobeGuesser.tsx
+++ src/components/views/GlobeGuesser.tsx
@@ -5,29 +5,59 @@ import GameInput from "components/ui/GameInput";
 import ScoreBoard from "components/ui/ScoreBoard";
 import BackgroundImage from "./sources/background.png";
 import { useLocation, useNavigate } from "react-router-dom";
+import { webSocketService } from './WebSocketService';
+import { api, handleError } from "helpers/api";
 
 const GlobeGuesser: React.FC = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const lat = queryParams.get('lat');
-  const long = queryParams.get('long');
+  const lat = queryParams.get('pong');
+  const long = queryParams.get('ping');
+  const round = parseInt(queryParams.get('round') || "0", 10);
   const [distance, setDistance] = useState<number | null>(null);
   const [timerTime, setTimerTime] = useState(120);
   const navigate = useNavigate();
 
-  localStorage.setItem("visited", true);
-
   const handleDistance = (calculatedDistance: number) => {
     setDistance(calculatedDistance);
     console.log("(GlobeGuesser) Distance from GameInput:", calculatedDistance);
-    navigate(`/lobby?distance=${calculatedDistance}`);
+    if (webSocketService.connected) {
+      const lobbyId = localStorage.getItem("lobby");
+      distanceSender(calculatedDistance, lobbyId);
+      console.log("(GlobeGuesser) Distance sent via WebSocket: " + calculatedDistance)
+    } else {
+      console.error("No connection (GlobeGuesser)");
+    }
   }
+
+  async function distanceSender(distance, lobbyId) {
+    try {
+      const userId = localStorage.getItem("userId");
+      const token = localStorage.getItem("token");
+      const requestBody = JSON.stringify({ "id": userId, "score": distance });
+      console.log(`(GlobeGuesserLobby) score to /Lobby/GameMode1/${lobbyId} with ${requestBody}`);
+      const headers = {
+        'Authorization': `${token}`
+      };
+      await api.put(`/Lobby/GameMode1/${lobbyId}`, requestBody, { headers });
+      navigate("/lobby");
+
+    } catch (error) {
+      console.error(`Failed to update score: ${handleError(error)}`);
+    }
+  }
+
+//when the timer is expired send default large distance
+const onTimeExpired = () => {
+  console.log("Timer expired, sending default distance");
+  handleDistance(100000000000000);
+};
 
   return (
     <BaseContainer backgroundImage={BackgroundImage} className="main-body">
       <div className={"center-container"}>
         <Header />
-        <ScoreBoard timerTime={timerTime} />
+        <ScoreBoard timerTime={timerTime} onTimeExpired={onTimeExpired} currentRound={round} totalRounds={5} />
         <GameInput lat={lat} long={long} onDistanceCalculated={handleDistance} />
       </div>
     </BaseContainer>
