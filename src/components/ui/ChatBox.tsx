654c4ff8e26f8cb0e0e64a25b3b4f497f9484927
whyKirbyy <timschmid02@icloud.com>
2024-05-17 14:42:18 +0200

    some changes to #53

--- src/components/ui/ChatBox.tsx
+++ src/components/ui/ChatBox.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import "../../styles/ui/ChatButton.scss";
 import { ReactComponent as CloseIcon } from "./sources/close.svg";
+import { stringToColor } from "helpers/colorHash.js";
 
 interface ChatBoxProps {
   className: string;
@@ -9,8 +10,10 @@ interface ChatBoxProps {
 
 const ChatBox: React.FC<ChatBoxProps> = ({ className, onClose }) => {
   const [messages, setMessages] = useState([
-    { name: "User1", message: "Hello!" },
-    { name: "User2", message: "Hi there!" },
+    { name: "Testuser123", message: "Hello!" },
+    { name: "username34", message: "Hi there!" },
+    { name: "thisisauser", message: "should we play globeguesser?" },
+    { name: "Testuser123", message: "sure" },
   ]);
   const [newMessage, setNewMessage] = useState("");
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
@@ -44,20 +47,20 @@ const ChatBox: React.FC<ChatBoxProps> = ({ className, onClose }) => {
       <div className="chat-box-messages">
         {messages.map((msg, index) => (
           <div key={index}>
-            <strong>{msg.name}:</strong> {msg.message}
+            <strong style={{ color: stringToColor(msg.name) }}>{msg.name}:</strong> {msg.message}
           </div>
         ))}
         <div ref={messagesEndRef} />
       </div>
       <div className="chat-box-input">
         <input
-                  ref={inputRef}
-                  type="text"
-                  value={newMessage}
-                  onChange={(e) => setNewMessage(e.target.value)}
-                  onKeyPress={handleKeyPress}
-                  placeholder="Type a message"
-                />
+          ref={inputRef}
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          onKeyPress={handleKeyPress}
+          placeholder="Type a message"
+        />
         <button onClick={handleSendMessage}>Send</button>
       </div>
     </div>
