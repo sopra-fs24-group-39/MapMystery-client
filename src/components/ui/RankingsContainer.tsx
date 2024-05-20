0f84e4ddd9f8cc9bc2d17cd9039c513d22eb23ff
whyKirbyy <timschmid02@icloud.com>
2024-05-20 08:09:54 +0200

    closes #84

--- src/components/ui/RankingsContainer.tsx
+++ src/components/ui/RankingsContainer.tsx
@@ -1,14 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import "../../styles/ui/RankingsContainer.scss";
-import { api} from "helpers/api";
+import { api } from "helpers/api";
 
 const BaseElementRankings: React.FC<BaseElementRankings> = ({ width, height }) => {
-   const [sortedFriends, setSortedFriends] = useState([]);
-   const [sortedPlayers, setSortedPlayers] = useState([]);
-   const [friends, setFriends] = useState([]);
+    const [sortedFriends, setSortedFriends] = useState([]);
+    const [sortedPlayers, setSortedPlayers] = useState([]);
+    const style = { width, height, overflowY: 'auto' };
+    const arrowStyle = { color: 'yellow' };
+    const username = localStorage.getItem('username');
 
     useEffect(() => {
-        async function fetchfriends() {
+        async function fetchFriendsAndCurrentUser() {
             try {
                 const token = localStorage.getItem("token");
                 const config = {
@@ -17,132 +19,147 @@ const BaseElementRankings: React.FC<BaseElementRankings> = ({ width, height }) =
                     }
                 };
                 const userId = localStorage.getItem("userId");
-                const response = await api.get(`/friends/${userId}`,config);
-                const fetchedFriends = response.data.map(friend => ({
+
+                const friendsResponse = await api.get(`/friends/${userId}`, config);
+                const fetchedFriends = friendsResponse.data.map(friend => ({
                     id: friend.id,
-                    name: friend.username, // Assuming the username needs to be mapped to name
+                    name: friend.username,
                     overall_points: friend.currentpoints,
-                    last30days_points: friend.pointsthismonth// Setting last30days_points to 0 for every friend
-                    // Add other attributes if needed
+                    last30days_points: friend.pointsthismonth
                 }));
-                setFriends(fetchedFriends);
-                setSortedFriends([...fetchedFriends]); // Update sortedFriends after friends have been set
+
+                const currentUserResponse = await api.get(`/users/${userId}`, config);
+                const currentUser = currentUserResponse.data;
+                const currentUserData = {
+                    id: currentUser.id,
+                    name: currentUser.username,
+                    overall_points: currentUser.currentpoints,
+                    last30days_points: currentUser.pointsthismonth
+                };
+
+                //adding current user to friends because section is called you and your friends
+                const combinedData = [...fetchedFriends, currentUserData];
+                const rankedFriends = assignRanks(combinedData);
+                setSortedFriends(rankedFriends);
             } catch (error) {
-                console.error('Failed to fetch friends:', error);
+                console.error('Failed to fetch friends or current user:', error);
             }
         }
+
         async function fetchPlayers() {
             try {
-                const userId = localStorage.getItem("userId");
-                // Fetch players data using your API
                 const response = await api.get(`/users`);
                 const fetchedPlayers = response.data.map(player => ({
                     id: player.id,
                     name: player.username,
                     overall_points: player.currentpoints,
                     last30days_points: player.pointsthismonth
-                    // Add other attributes if needed
                 }));
-                setSortedPlayers([...fetchedPlayers]); // Update sortedPlayers with fetched player data
+                const rankedPlayers = assignRanks(fetchedPlayers);
+                setSortedPlayers(rankedPlayers);
             } catch (error) {
                 console.error('Failed to fetch players:', error);
             }
         }
-        fetchfriends();
+
+        fetchFriendsAndCurrentUser();
         fetchPlayers();
     }, []);
 
-
-   const [sortCriteria, setSortCriteria] = useState({
-     friends: {
-       overall_points: { direction: 'up' },
-       last30days_points: { direction: 'up' }
-     },
-     global: {
-       overall_points: { direction: 'up' },
-       last30days_points: { direction: 'up' }
-     }
-   });
-
-   const sortData = (dataset, setDataset, group, key) => {
-     const isDescending = sortCriteria[group][key].direction === 'down';
-     const sortedArray = [...dataset].sort((a, b) => isDescending ? b[key] - a[key] : a[key] - b[key]);
-     setDataset(sortedArray);
-     setSortCriteria(prev => ({
-       ...prev,
-       [group]: {
-         ...prev[group],
-         [key]: { direction: isDescending ? 'up' : 'down' }
+    const [sortCriteria, setSortCriteria] = useState({
+        friends: {
+            overall_points: { direction: 'down' },
+            last30days_points: { direction: 'up' }
+        },
+        global: {
+            overall_points: { direction: 'down' },
+            last30days_points: { direction: 'up' }
         }
-     }));
-   };
+    });
 
-   const style = { width, height, overflowY: 'auto' };
-   const arrowStyle = { color: 'yellow' };
+    const sortData = (dataset, setDataset, group, key) => {
+        const isDescending = sortCriteria[group][key].direction === 'down';
+        const sortedArray = [...dataset].sort((a, b) => isDescending ? a[key] - b[key] : b[key] - a[key]);
+        setDataset(sortedArray);
+        setSortCriteria(prev => ({
+            ...prev,
+            [group]: {
+                ...prev[group],
+                [key]: { direction: isDescending ? 'up' : 'down' }
+            }
+        }));
+    };
 
-   return (
-     <div className="base-element-rankings" style={style}>
-       <div className="full-width-container">
-         <div className="title-row">
-           You and your friends:
-           <div className="button-cont">
-             <button className="overall_button" onClick={() => sortData(sortedFriends, setSortedFriends, 'friends', 'overall_points')}>
-               Overall
-               <span style={arrowStyle}>
-                 {sortCriteria.friends.overall_points.direction === 'down' ? '▼' : '▲'}
-               </span>
-             </button>
-             <button className="overall_button" onClick={() => sortData(sortedFriends, setSortedFriends, 'friends', 'last30days_points')}>
-               This Month
-               <span style={arrowStyle}>
-                 {sortCriteria.friends.last30days_points.direction === 'down' ? '▼' : '▲'}
-               </span>
-             </button>
-           </div>
-         </div>
-         {sortedFriends.map((player, index) => (
-           <div
-             key={player.id}
-             className={`player-row ${index % 2 === 0 ? 'light-stripe' : 'dark-stripe'} ${player.id === 2 ? 'highlighted-row' : ''}`}
-           >
-             <div className="player-id">#{player.id}</div>
-             <div className="player-name">{player.name}</div>
-             <div className="player-overall">{player.overall_points}</div>
-             <div className="player-last30days">{player.last30days_points}</div>
-           </div>
-         ))}
-         <div className="title-row">
-           Global Ranking:
-           <div className="button-cont">
-             <button className="overall_button" onClick={() => sortData(sortedPlayers, setSortedPlayers, 'global', 'overall_points')}>
-               Overall
-               <span style={arrowStyle}>
-                 {sortCriteria.global.overall_points.direction === 'down' ? '▼' : '▲'}
-               </span>
-             </button>
-             <button className="overall_button" onClick={() => sortData(sortedPlayers, setSortedPlayers, 'global', 'last30days_points')}>
-               This Month
-               <span style={arrowStyle}>
-                 {sortCriteria.global.last30days_points.direction === 'down' ? '▼' : '▲'}
-               </span>
-             </button>
-           </div>
-         </div>
-         {sortedPlayers.map((player, index) => (
-           <div
-             key={player.id}
-             className={`player-row ${index % 2 === 0 ? 'light-stripe' : 'dark-stripe'} ${player.id === 2 ? 'highlighted-row' : ''}`}
-           >
-             <div className="player-id">#{player.id}</div>
-             <div className="player-name">{player.name}</div>
-             <div className="player-overall">{player.overall_points}</div>
-             <div className="player-last30days">{player.last30days_points}</div>
-           </div>
-         ))}
-       </div>
-     </div>
-   );
+    const assignRanks = (players) => {
+        const sortedByOverallPoints = [...players].sort((a, b) => b.overall_points - a.overall_points);
+        return sortedByOverallPoints.map((player, index) => ({
+            ...player,
+            rank: index + 1
+        }));
+    };
+
+    return (
+        <div className="base-element-rankings" style={style}>
+            <div className="full-width-container">
+                <div className="title-row">
+                    You and your friends:
+                    <div className="button-cont">
+                        <button className="overall_button" onClick={() => sortData(sortedFriends, setSortedFriends, 'friends', 'overall_points')}>
+                            Overall
+                            <span style={arrowStyle}>
+                                {sortCriteria.friends.overall_points.direction === 'down' ? '▼' : '▲'}
+                            </span>
+                        </button>
+                        <button className="overall_button" onClick={() => sortData(sortedFriends, setSortedFriends, 'friends', 'last30days_points')}>
+                            This Month
+                            <span style={arrowStyle}>
+                                {sortCriteria.friends.last30days_points.direction === 'down' ? '▼' : '▲'}
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                {sortedFriends.map((player) => (
+                    <div
+                        key={player.id}
+                        className={`player-row ${player.rank % 2 === 0 ? 'light-stripe' : 'dark-stripe'} ${player.name === username ? 'highlighted-row' : ''}`}
+                    >
+                        <div className="player-id">#{player.rank}</div>
+                        <div className="player-name">{player.name}</div>
+                        <div className="player-overall">{player.overall_points}</div>
+                        <div className="player-last30days">{player.last30days_points}</div>
+                    </div>
+                ))}
+                <div className="title-row">
+                    Global Ranking:
+                    <div className="button-cont">
+                        <button className="overall_button" onClick={() => sortData(sortedPlayers, setSortedPlayers, 'global', 'overall_points')}>
+                            Overall
+                            <span style={arrowStyle}>
+                                {sortCriteria.global.overall_points.direction === 'down' ? '▼' : '▲'}
+                            </span>
+                        </button>
+                        <button className="overall_button" onClick={() => sortData(sortedPlayers, setSortedPlayers, 'global', 'last30days_points')}>
+                            This Month
+                            <span style={arrowStyle}>
+                                {sortCriteria.global.last30days_points.direction === 'down' ? '▼' : '▲'}
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                {sortedPlayers.map((player) => (
+                    <div
+                        key={player.id}
+                        className={`player-row ${player.rank % 2 === 0 ? 'light-stripe' : 'dark-stripe'} ${player.name === username ? 'highlighted-row' : ''}`}
+                    >
+                        <div className="player-id">#{player.rank}</div>
+                        <div className="player-name">{player.name}</div>
+                        <div className="player-overall">{player.overall_points}</div>
+                        <div className="player-last30days">{player.last30days_points}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default BaseElementRankings;
-
